750d156983a679d27527121ece0e9b6b2d503d6e
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-06-26 16:26:05 +0900

    add PalindromeNumber.java

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,20 @@
+
+
+
+/**
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ * < số nguyên tố Palindrome hay số xuôi ngược nguyên tố là số nguyên tố viết xuôi hay viết ngược vẫn chỉ cho ra một số >
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * */
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        
+
+        return  false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
