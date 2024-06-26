c7c50d1b45a4bda6c9bd818b67772806eb0919fc
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-06-26 17:53:53 +0900

    upload file

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,6 +1,4 @@
-
-
-
+import java.util.Scanner;
 /**
  * Given an integer x, return true if x is a palindrome, and false otherwise.
  * < số nguyên tố Palindrome hay số xuôi ngược nguyên tố là số nguyên tố viết xuôi hay viết ngược vẫn chỉ cho ra một số >
@@ -9,12 +7,20 @@
  * Explanation: 121 reads as 121 from left to right and from right to left.
  * */
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
-        
 
-        return  false;
+    // chia so lam 2 chuoi ngang nhau va so sanh
+    public boolean isPalindrome(int x) {
+        if(x< 0 || (x % 10 ==0 && x!=0)) return false;
+        else{
+            int  reversedHalf = 0;
+            while (x > reversedHalf){
+                reversedHalf = (reversedHalf * 10) + (x % 10);
+                x = x/10;
+            }
+            return  x==reversedHalf ||x == reversedHalf/10;
+        }
     }
     public static void main(String[] args) {
-
+        System.out.println(new PalindromeNumber().isPalindrome(new Scanner(System.in).nextInt()));
     }
 }
